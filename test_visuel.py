591cb18e303089f99b2212e6e0e81aa369c1cc5b
Quent1-G <qgrizard@gmail.com>
2025-02-12 17:25:12 +0100

    Update test_visuel.py

--- test_visuel.py
+++ test_visuel.py
@@ -2,21 +2,25 @@ import streamlit as st
 
 # Fonction principale pour l'application
 def app():
+    ########################################################### TITRE ###################################
     # Titre de la page principale
-    st.title("Mon Application Streamlit avec Bandeau Latéral Défilant")
-
+    st.title("Ce qu'on met dans nos assietes, quel impact sur la planète ?")
+    #####################################################################################################
     # Créer une mise en page avec un conteneur de deux colonnes
     col1, col2 = st.columns([1, 4])  # col1 pour le bandeau, col2 pour le contenu principal
 
     # Contenu du bandeau latéral
     with col1:
-        st.sidebar.title("Options de Menu")
-        st.sidebar.radio("Choisissez une option", ["Option 1", "Option 2", "Option 3"])
-        st.sidebar.write("Plus de détails ici...")
-
+        ################################################## CONTENU BANDEAU ###########################
+        st.sidebar.title("Contenu")
+        st.sidebar.radio("Navigation", ["Accueil", "Contexte", "Méthodologie","Analyse globale"])
+        st.sidebar.write("A propos :")
+        st.sidebar.write("nous étudiants et bla bla bla...")
+        ########################################### CONTENU PAGE ######################################
     # Contenu principal de la page
-    with col2:
+    with col1:
         st.write("Voici le contenu de la page principale qui peut être défilé.")
+    with col2:
         st.write("Ajoutez du contenu ici, des graphiques, des tableaux, etc.")
         for i in range(30):
             st.write(f"Exemple de contenu {i+1}")
