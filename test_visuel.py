997b768b27a1f56cb27206de0d4aa668c5db2f16
Quent1-G <qgrizard@gmail.com>
2025-02-12 17:44:26 +0100

    Update test_visuel.py

--- test_visuel.py
+++ test_visuel.py
@@ -25,8 +25,7 @@ st.markdown(
 def app():
     
     ########################################################################### Titre de la page principale######################################################################
-    st.title("Ce qu'on met dans nos assiettes...")
-    st.title("Quel impact sur la planète ?")
+    st.title("Ce qu'on met dans nos assiettes...\### Quel impact sur la planète ?")
     ####################################################################### Bandeau latéral######################################################################
     st.sidebar.title("Contenu")
     st.sidebar.radio("Navigation", ["Accueil", "Contexte", "Méthodologie", "Analyse globale"])
