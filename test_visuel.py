5eb3eda5c0f5df5648471830bb8e30a29ba3ce9a
Quent1-G <qgrizard@gmail.com>
2025-02-12 17:35:42 +0100

    Update test_visuel.py

--- test_visuel.py
+++ test_visuel.py
@@ -6,17 +6,13 @@ def app():
     # Titre de la page principale
     st.title("Ce qu'on met dans nos assietes, quel impact sur la planète ?")
     #####################################################################################################
-    # Créer une mise en page avec un conteneur de deux colonnes
-    col1, col2 = st.columns([1, 4])  # col1 pour le bandeau, col2 pour le contenu principal
-
-    # Contenu du bandeau latéral
-    with col1:
-        ################################################## CONTENU BANDEAU ###########################
-        st.sidebar.title("Contenu")
-        st.sidebar.radio("Navigation", ["Accueil", "Contexte", "Méthodologie","Analyse globale"])
-        st.sidebar.write("A propos :")
-        st.sidebar.write("nous étudiants et bla bla bla...")
-        ########################################### CONTENU PAGE ######################################
+     # Contenu du bandeau latéral
+    ################################################## CONTENU BANDEAU ###########################
+    st.sidebar.title("Contenu")
+    st.sidebar.radio("Navigation", ["Accueil", "Contexte", "Méthodologie","Analyse globale"])
+    st.sidebar.write("A propos :")
+    st.sidebar.write("nous étudiants et bla bla bla...")
+    ########################################### CONTENU PAGE ######################################
     # Contenu principal de la page
     st.write("aujourd'hui, bla bla bla, la conso et la planete bla bal bla")
 # Lancer l'application
