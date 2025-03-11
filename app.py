615a81bd1b575ef67104d9ce7ecd3fe6f9f914b0
Quent1-G <qgrizard@gmail.com>
2025-03-11 14:39:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,7 @@ gerer_panier()
 
 # Affichage des codes CIQUAL dans le panier
 codes_ciqual = [int(produit["code_ciqual"]) for produit in st.session_state.panier]
-if codes_ciqual:
-    st.write("Liste des codes CIQUAL dans le panier :", codes_ciqual)
-else:
-    st.write("Il n'y a pas de codes CIQUAL dans le panier.")
+
 
 # Appeler la fonction pour calculer le score du panier
 score_panier()
