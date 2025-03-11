abc4e727471e9909744c06eb3d7fc767e9d88c74
Quent1-G <qgrizard@gmail.com>
2025-03-11 11:18:34 +0100

    Update test2.py

--- test2.py
+++ test2.py
@@ -3,3 +3,9 @@ from Panier import gerer_panier
 
 # Appeler la fonction qui gère tout le panier
 gerer_panier()
+# Affichage des codes CIQUAL dans le panier
+codes_ciqual = [produit["code_ciqual"] for produit in st.session_state.panier]
+if codes_ciqual:
+  st.write("Liste des codes CIQUAL dans le panier :", codes_ciqual)
+else:
+  st.write("Il n'y a pas de codes CIQUAL dans le panier.")
