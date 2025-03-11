22584e53b56e6d21e2945d0609ef4fad9eff9689
Quent1-G <qgrizard@gmail.com>
2025-03-11 15:46:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from Panier import gerer_panier
 from Score_panier import score_panier
+from Variables import variables
 
 # Appeler la fonction qui gère tout le panier
 gerer_panier()
@@ -11,3 +12,6 @@ codes_ciqual = [int(produit["code_ciqual"]) for produit in st.session_state.pani
 
 # Appeler la fonction pour calculer le score du panier
 score_panier()
+
+# Appeler la fonction pour afficher les variables environnementales
+variables()
